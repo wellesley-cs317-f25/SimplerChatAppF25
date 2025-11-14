ffb0b05fc8430df62e5c79535c860cdb9ae912ba
Franklyn Turbak <fturbak@wellesley.edu>
2025-11-14 08:57:11 -0500

    Forgot to commit a few files for first working pscreenAuthNonPersistent branch

---
+++ components/ChatViewPScreen-authStub.js
@@ -0,0 +1,26 @@
+/**
+ * This is just a stub for an ChatView pseudoScreen for SimplerChatApp
+ * that just displays text saying it's a stub.
+ */
+
+import { Text, View } from 'react-native';
+import styles from '../styles';
+
+import { useSignedInUser } from '../firebaseInit-nonPersistentAuth';
+
+export default function ChatViewPScreen( { changePscreen } ) {
+  const signedInUser = useSignedInUser();
+  return (
+    <View style={ styles.pscreen }>
+      <Text style={ styles.pscreenText }>
+      This is just a stub for a pseudoScreen that displays chat messages.
+      {"\n"}
+      { signedInUser?.email ?
+	`The signed-in user's email is ${signedInUser?.email}.` :
+	`No user is signed in.`}
+      </Text>
+    </View>
+  );
+}
+
+
