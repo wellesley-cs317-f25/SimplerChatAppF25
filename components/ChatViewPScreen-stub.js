10b165a44ecb0f731c2cd1c607b03e6b9a958d65
Franklyn Turbak <fturbak@wellesley.edu>
2025-11-14 09:09:51 -0500

    Added latest components/ChatViewPScreen-stub.js

--- components/ChatViewPScreen-stub.js
+++ components/ChatViewPScreen-stub.js
@@ -9,8 +9,10 @@ import styles from '../styles';
 
 export default function ChatViewPScreen( { changePscreen } ) {
   return (
-    <View style={ styles.screen }>
-      <Text>This is just a stub for a pseudoScreen that displays chat messages.</Text>  
+    <View style={ styles.pscreen }>
+      <Text style={ styles.pscreenText }>
+        This is just a stub for a pseudoScreen that displays chat messages.
+      </Text>  
     </View>
   );
 }
